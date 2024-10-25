8d3fe1cdc5ee913745310db2474db5433eb3a364
Alexxkowal <alexxkowal@mail.ru>
2024-10-25 14:02:38 +0400

    Task7

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -4,9 +4,12 @@ import org.knit.lab2.task3.Task3;
 import org.knit.lab2.task4.Task4;
 import org.knit.lab2.task5.Task5;
 import org.knit.lab3.*;
+import org.knit.lab4.Task7;
+
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 //        Task1 task1 = new Task1();
 //        task1.execute();
 //
@@ -15,12 +18,14 @@ public class Main {
 
 //        Task3 task3 = new Task3();
 //        task3.execute();
-        Task4 task4 = new Task4();
-        task4.execute();
-//          Task6 task6 = new Task6();
-//          task6.execute();
-        Task5 task5 = new Task5();
-        Task5.execute();
+//        Task4 task4 = new Task4();
+//        task4.execute();
+////          Task6 task6 = new Task6();
+////          task6.execute();
+//        Task5 task5 = new Task5();
+//        Task5.execute();
+        Task7 task7 = new Task7();
+        task7.execute();
 
     }
 }
--- src/main/java/org/knit/lab4/Task7.java
+++ src/main/java/org/knit/lab4/Task7.java
@@ -1,21 +1,36 @@
-//package org.knit.lab4;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.ArrayList;
-//import java.util.Scanner;
-//
-//public class Task7 {
-//    public static void main(String[] args) throws FileNotFoundException {
-//    Scanner scanner = new Scanner(new File("src/main/java/org/knit/lab4/dictionary.txt"));
-//    ArrayList <String> arrayList = new ArrayList<>();
-//    while (scanner.hasNext()) {
-//        String word = scanner.nextLine();
-//        arrayList.add(word);
-//    }
-//    String[] array = arrayList.toArray(new String[0]);
-//    DictionaryStatistics dictionaryStatistics = new DictionaryStatistics(array);
-//    System.out.println(dictionaryStatistics);
-//    scanner.close();
-//}
-//}
+package org.knit.lab4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task7 {
+    public static void execute() throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File("src/main/java/org/knit/lab4/dictionary.txt"));
+    ArrayList <String> arrayList = new ArrayList<>();
+    while (scanner.hasNext()) {
+        String word = scanner.nextLine();
+        arrayList.add(word);
+    }
+    char[] russianAlphabet = new char[32];
+
+        // Используем цикл for для добавления букв алфавита
+        for (char ch = 'а'; ch <= 'я'; ch++) {
+            russianAlphabet[ch - 'а'] = ch;  // Заполняем массив
+        }
+    String[] array = arrayList.toArray(new String[0]);
+    DictionaryStatistics dictionaryStatistics = new DictionaryStatistics(array, russianAlphabet);
+    scanner.close();
+    System.out.println(dictionaryStatistics.getPolindrom());
+    System.out.println(dictionaryStatistics.getMaxWordLength());
+    dictionaryStatistics.printAlphabet();
+    System.out.println(dictionaryStatistics.getDictionarySize());
+    dictionaryStatistics.printSymbolStat();
+    System.out.println(dictionaryStatistics.getRandomWord());
+    dictionaryStatistics.wordGame("крот");
+
+}
+}
